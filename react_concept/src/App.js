39b7b9a08a01b21cbe1a2a985495c98d7b31167b
LikithaDavala <likithadavala59@gmail.com>
2026-09-08 23:32:07 +0530

    App Pages

--- react_concept/src/App.js
+++ react_concept/src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import Register from "./Pages/Register"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './Pages/Register';
+import Loginpage from './Pages/loginpage';
 
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      
-        <Route path='/' element={<Register />}/>
+      <Routes>
+        <Route path='/Register' element={<Register />} />
+        <Route path='/' element={<Loginpage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
