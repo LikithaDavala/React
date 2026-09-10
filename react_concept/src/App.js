6a75fe17727479592e02bcf84842cd2b90ea2733
LikithaDavala <likithadavala59@gmail.com>
2026-09-10 17:19:35 +0530

    App

--- react_concept/src/App.js
+++ react_concept/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './Pages/Register';
 import Loginpage from './Pages/loginpage';
 
-import './App.css';
 
 function App() {
   return (
