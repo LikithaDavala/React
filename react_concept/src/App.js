eb244b0b9e4197d8be377176ea312233bd84b4df
LikithaDavala <likithadavala59@gmail.com>
2026-09-11 20:34:39 +0530

    App

--- react_concept/src/App.js
+++ react_concept/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './Pages/Register';
 import Loginpage from './Pages/loginpage';
+import Users from './Pages/users';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/Register' element={<Register />} />
         <Route path='/' element={<Loginpage />} />
+        <Route path='/users' element={<Users />} />
       </Routes>
     </BrowserRouter>
   );
