870be4e7caa7a26b3e66a9b86de0f3711028a553
LikithaDavala <likithadavala59@gmail.com>
2026-09-12 16:47:19 +0530

    Product Page

---
+++ react_concept/src/Pages/product.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import productAPI from "../api/product";
+import Products  from "../component/product";
+
+function Product() {
+    const [projectValue, projectInput] = useState("");
+    const [collectionValue, collectionInput] = useState("");
+    const [productlist, setproductlist] = useState([]);
+
+    productAPI().then((response) => {
+        return response.json();
+    }).then((data) => {
+        projectInput(data.project);
+        collectionInput(data.collection)
+        setproductlist(data.products);
+    }).catch((error) => {
+        console.error(error)
+    })
+
+
+    return (
+        <div>
+            <div>
+                <label>Project Name:</label>
+                <input
+                    value={projectValue}
+                    onChange={(e) => projectInput(e.target.value)}
+                />
+            </div>
+
+
+            <div>
+                <label>Project Name:</label>
+                {projectValue}
+                <br />
+                <label>Collection Name:</label>
+                {collectionValue}
+            </div>
+
+         <Products productlist={productlist}/>
+        </div>
+    )
+}
+export default Product;
