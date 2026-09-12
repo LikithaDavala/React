256d679c7d7e8cd2b7f252024aeb4398065679f7
LikithaDavala <likithadavala59@gmail.com>
2026-09-12 16:45:38 +0530

    Detail Page in Component

--- react_concept/src/component/details.jsx
+++ react_concept/src/component/details.jsx
@@ -1,16 +1,16 @@
-function Details({ users}) {
+function Details({ users = [] }) {
   return (
     <div>
-  
-    
-      {users.map((u) => (
-        <div>
-          <p>Name: {u.name}</p>
-          <p>Phone Number: {u.phoneNumber}</p>
-          <p>Email: {u.email}</p>
+      <p>detail component</p>
+      {users.map((user) => (
+        <div key={user.email}>
+          <p>Name: {user.name}</p>
+          <p>Phone Number: {user.phoneNumber}</p>
+          <p>Email: {user.email}</p>
         </div>
-      ))} 
+      ))}
     </div>
   );
 }
+
 export default Details;
