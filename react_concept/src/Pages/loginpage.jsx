09915141a20461b65c186e78aeddf4f25cfe906b
LikithaDavala <likithadavala59@gmail.com>
2026-09-12 16:47:39 +0530

    Login Page

--- react_concept/src/Pages/loginpage.jsx
+++ react_concept/src/Pages/loginpage.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import Details from "../component/details";
 
 function Loginpage(){
 const [phone ,input]=useState("");
 
+const users = [
+        { name: "kavya", phoneNumber: "123-456-7890", email: "kavya@gmail.com" },
+        { name: "sujana", phoneNumber: "987-654-3210", email: "sujana@gmail.com" }
+    ];
 
     return(
         <div>
@@ -15,6 +20,8 @@ const [phone ,input]=useState("");
             <input
             value={phone}
             />
+            <p>login page </p>
+            <Details users={users}/>
         </div>
         
         
