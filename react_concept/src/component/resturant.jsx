923e21f1cc56070baa8628f0184ee910888ae28f
LikithaDavala <likithadavala59@gmail.com>
2026-09-13 23:58:47 +0530

    Restuarnt

---
+++ react_concept/src/component/resturant.jsx
@@ -0,0 +1,20 @@
+function Resturants({ productlist }) {
+    return (
+
+        <div>
+            {productlist.map((prod) => {
+                return (
+                    <div key={prod.id}>
+                        <label>Item Name:</label>
+                        {prod.itemname}
+                        <label>item category:</label>
+                        {prod.itemcategory}
+                        <br />
+                    </div>
+                )
+            })}
+        </div>
+    )
+
+
+} export default Resturants;
