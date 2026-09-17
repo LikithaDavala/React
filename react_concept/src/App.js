3d04401d5805e997b810a66a01dc61afc34d42cc
LikithaDavala <likithadavala59@gmail.com>
2026-09-17 20:43:58 +0530

    App

--- react_concept/src/App.js
+++ react_concept/src/App.js
@@ -3,6 +3,7 @@ import Register from './Pages/Register';
 import Loginpage from './Pages/loginpage';
 import Users from './Pages/users';
 import Product from'./Pages/product';
+import Resturant from './Pages/resturant';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Loginpage />} />
         <Route path='/users' element={<Users />} />
          <Route path='/product' element={<Product />} />
+         <Route path='/Resturant' element={<Resturant/>} />
       </Routes>
     </BrowserRouter>
   );
